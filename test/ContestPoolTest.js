7107c9a7c5aed35e37bd43b502e365c9cc624996
Douglas Molina <doug.molina@coralbytes.com>
2018-03-04 18:45:26 -0600

    add unit test structure for contributions

--- test/ContestPoolTest.js
+++ test/ContestPoolTest.js
@@ -4,12 +4,14 @@ const moment = require('moment');
 // test suite
 contract('ContestPool', accounts => {
     let contestPoolInstance;
-    let owner = accounts[0];
-    let manager = accounts[1];
+    let owner = accounts[9];
+    let manager = accounts[0];
+    let player1 = accounts[1];
+    let player2 = accounts[2];
+    let player3 = accounts[3];
     let startTime = moment("2018-06-14").toDate().getMilliseconds();
     let endTime = moment("2018-07-16").toDate().getMilliseconds();
     let graceTime = 1;
-    console.log(startTime);
 
     beforeEach('setup contract for each test', async() => {
         contestPoolInstance = await ContestPool.new(
@@ -30,4 +32,10 @@ contract('ContestPool', accounts => {
         assert.equal(endTime, endTimeContract, "Contest end time should be " + endTime);
         assert.equal(graceTime, graceTimeContract, "Contest grace time should be " + graceTime);
     });
+
+    it('should take contributions from players', async () => {
+        
+    });
+
+
 });
