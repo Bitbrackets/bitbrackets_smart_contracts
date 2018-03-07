9d3ff652dc22fc662e0275a6e69d4dd37207832e
dtutila <daniel.tutila@gmail.com>
2018-03-06 22:40:38 -0600

    - Adding to gitignore .idea configuration directory - Adding method to claim prize by valid winner after grace time has passed

--- contracts/ContestPool.sol
+++ contracts/ContestPool.sol
@@ -1,20 +1,23 @@
 pragma solidity ^0.4.18;
 
 import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
 
 
 contract ContestPool is Ownable {
+    using SafeMath for uint256;
+
     address public manager;
     bytes32 public contestName;
     uint public startTime;
     uint public endTime;
     uint public graceTime;
-
-    mapping(address => uint) public predictions;
-    mapping(address => uint) public participantPoints;
     uint public numberOfParticipants;
     uint public maxBalance;
-    
+
+    mapping(address => uint) public predictions;
+    mapping(address => uint) private winners;
+
     function ContestPool(
         address _owner, 
         address _manager, 
@@ -23,7 +26,8 @@ contract ContestPool is Ownable {
         uint _endTime,
         uint _graceTime,
         uint _maxBalance
-    ) public {
+    ) public 
+    {
         owner = _owner;
         manager = _manager;
         contestName = _contestName;
@@ -33,12 +37,49 @@ contract ContestPool is Ownable {
         maxBalance = _maxBalance;
     }
 
+    /**
+    * @dev this function is used for a winner to claim the prize
+    *
+    **/
+    function claimThePrize() external returns(bool) {
+        require(winners[msg.sender] > 0);
+        require(getTimestamp().sub(endTime) > graceTime);
+
+        uint prize = winners[msg.sender];
+
+        require(this.balance > prize);
+
+        winners[msg.sender] = 0;
+
+        if (!msg.sender.send(prize)) {
+            winners[msg.sender] = prize;
+            return false;
+        }
+
+        return true;
+        
+    }
+
     function sendPrediction(uint prediction) public payable {
         require(prediction > 0);
         require(predictions[msg.sender] == 0);
         predictions[msg.sender] = prediction;
     }
 
+    function addWinner(address winnerAddress, uint256 prize) public returns(bool){
+        winners[winnerAddress] = prize;
+        return true;
+    }
+
+    function addressHasPrize () public view returns (bool res) {
+
+        return winners[msg.sender] > 0;
+    }
+    
+    function getTimestamp() public view returns (uint256)
+    { 
+        return now; 
+    }
 
 
 }
