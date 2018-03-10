f91dc0d48fd6ef73794fd2bcea0686a9232ea579
dtutila <daniel.tutila@gmail.com>
2018-03-09 23:07:18 -0600

    - updating code format

--- test/ContestPoolFactoryTest.js
+++ test/ContestPoolFactoryTest.js
@@ -6,115 +6,117 @@ var utils = require("./utils.js");
 
 let instance;
 
-contract('ContestPoolFactory', function(accounts) {
-  
-  beforeEach('setup contract for each test', async() => {
-    instance = await ContestPoolFactory.deployed();
-  });
+contract('ContestPoolFactory', function (accounts) {
 
-  it("Should deploy ContestPoolFactory contract.", async function() {
-    assert(instance);
-    assert(instance.address);
-  });
+    beforeEach('setup contract for each test', async () => {
+        instance = await ContestPoolFactory.deployed();
+    });
 
-  it("Creating a contest pool definition.", async function() {
-    const contestName = 'ContestPool';
-    const startTime = 1000;
-    const endTime = 2000;
-    const graceTime = 2;
-    const maxBalance = web3.toWei(10,'ether');
+    it("Should deploy ContestPoolFactory contract.", async function () {
+        assert(instance);
+        assert(instance.address);
+    });
 
-    await instance.createContestPoolDefinition(contestName, startTime, endTime, graceTime, maxBalance);
+    it("Creating a contest pool definition.", async function () {
+        const contestName = 'ContestPool';
+        const startTime = 1000;
+        const endTime = 2000;
+        const graceTime = 2;
+        const maxBalance = web3.toWei(10, 'ether');
 
-    const result = await instance.definitions(contestName);
-    
-    assert.equal(contestName, stringUtils.cleanNulls(web3.toAscii(result[0])));
-    assert.equal(startTime, result[1]);
-    assert.equal(endTime, result[2]);
-    assert.equal(graceTime, result[3]);
-  });
+        await instance.createContestPoolDefinition(contestName, startTime, endTime, graceTime, maxBalance);
 
-  it("Creating a contest pool definition twice. It should fail.", async function() {
-    const contestName = 'NewContestPool';
-    const startTime = 1000;
-    const endTime = 2000;
-    const graceTime = 2;
-    const maxBalance = web3.toWei(10,'ether');
-    await instance.createContestPoolDefinition(contestName, startTime, endTime, graceTime, maxBalance);
+        const result = await instance.definitions(contestName);
 
-    try {
-      await instance.createContestPoolDefinition(contestName, startTime, endTime, graceTime, maxBalance);
-      assert(false, 'It should have failed because the contest name is repetead.');
-    } catch(err) {
-      assert(err);
-    }
-  });
+        assert.equal(contestName, stringUtils.cleanNulls(web3.toAscii(result[0])));
+        assert.equal(startTime, result[1]);
+        assert.equal(endTime, result[2]);
+        assert.equal(graceTime, result[3]);
+    });
 
-  it("Creating a contest pool definition with an invalid value for contest name. It should fail.", async function() {
-    try {
-      await instance.createContestPoolDefinition(null, 1, 2, 2, 10);
-      assert(false, 'It should have failed because the contest name is invalid.');
-    } catch(err) {
-      assert(err);
-    }
-  });
+    it("Creating a contest pool definition twice. It should fail.", async function () {
+        const contestName = 'NewContestPool';
+        const startTime = 1000;
+        const endTime = 2000;
+        const graceTime = 2;
+        const maxBalance = web3.toWei(10, 'ether');
+        await instance.createContestPoolDefinition(contestName, startTime, endTime, graceTime, maxBalance);
 
-  it("Creating a contest pool definition with an invalid value for start date. It should fail.", async function() {
-    try {
-      await instance.createContestPoolDefinition('CustomValue', 0, 2, 2, 10);
-      assert(false, 'It should have failed because the start date is zero.');
-    } catch(err) {
-      assert(err);
-    }
-  });
+        try {
+            await instance.createContestPoolDefinition(contestName, startTime, endTime, graceTime, maxBalance);
+            assert(false, 'It should have failed because the contest name is repetead.');
+        } catch (err) {
+            assert(err);
+        }
+    });
 
-  
-  it("Creating a contest pool instance. Should create event", async function() {
-    const contestName = 'Rusia18';
-    const startTime = 1000;
-    const endTime = 2000;
-    const graceTime = 2;
-    const maxBalance = web3.toWei(1,'ether');
-    const contestNameBytes32 = stringUtils.stringToBytes32(contestName);
+    it("Creating a contest pool definition with an invalid value for contest name. It should fail.", async function () {
+        try {
+            await instance.createContestPoolDefinition(null, 1, 2, 2, 10);
+            assert(false, 'It should have failed because the contest name is invalid.');
+        } catch (err) {
+            assert(err);
+        }
+    });
 
-    await instance.createContestPoolDefinition(contestName, startTime, endTime, graceTime, maxBalance);
-    
-    await instance.createContestPool(contestName, web3.toWei(2, 'ether'));
+    it("Creating a contest pool definition with an invalid value for start date. It should fail.", async function () {
+        try {
+            await instance.createContestPoolDefinition('CustomValue', 0, 2, 2, 10);
+            assert(false, 'It should have failed because the start date is zero.');
+        } catch (err) {
+            assert(err);
+        }
+    });
 
-    let contestPoolAddress;
-    let contestPool;
 
-    const callback = async function(log) {
-      contestPoolAddress = log[0].args.contestPoolAddress;
-      contestPool = ContestPool.at(contestPoolAddress);
+    it("Creating a contest pool instance. Should create event", async function () {
+        const contestName = 'Rusia18';
+        const startTime = 1000;
+        const endTime = 2000;
+        const graceTime = 2;
+        const maxBalance = web3.toWei(1, 'ether');
+        const contestNameBytes32 = stringUtils.stringToBytes32(contestName);
 
-      assert.ok(contestPoolAddress);
-      assert.ok(contestPool);
+        await instance.createContestPoolDefinition(contestName, startTime, endTime, graceTime, maxBalance);
 
-      const maxBalanceContestPool = await contestPool.maxBalance();
-      const contestNameContestPool = await contestPool.contestName();
-      const startDateContestPool = await contestPool.startTime();
-      const endDateContestPool = await contestPool.endTime();
-      const daysGraceContestPool = await contestPool.graceTime();
+        await instance.createContestPool(contestName, web3.toWei(2, 'ether'));
 
-      assert.equal(maxBalanceContestPool, maxBalance);
-      assert.equal(contestNameContestPool, contestNameBytes32);
-      assert.equal(startDateContestPool, startTime);
-      assert.equal(endDateContestPool, endTime);
-      assert.equal(maxBalanceContestPool, maxBalance);
-    };
-    await utils.assertEvent(instance, { event: "CreateContestPool", args: {
-      contestName: contestNameBytes32
-    }}, 1, callback);
-  });
+        let contestPoolAddress;
+        let contestPool;
 
-  it("Creating a contest pool instance using invalid contest name.", async function() {
-    const contestName = 'Rusia21';
-    try {
-      await instance.createContestPool(contestName, web3.toWei(2, 'ether'));
-      assert(false, 'It should fail because contest name is invalid.');
-    } catch(err) {
-      assert(err);
-    }    
-  });
+        const callback = async function (log) {
+            contestPoolAddress = log[0].args.contestPoolAddress;
+            contestPool = ContestPool.at(contestPoolAddress);
+
+            assert.ok(contestPoolAddress);
+            assert.ok(contestPool);
+
+            const maxBalanceContestPool = await contestPool.maxBalance();
+            const contestNameContestPool = await contestPool.contestName();
+            const startDateContestPool = await contestPool.startTime();
+            const endDateContestPool = await contestPool.endTime();
+            const daysGraceContestPool = await contestPool.graceTime();
+
+            assert.equal(maxBalanceContestPool, maxBalance);
+            assert.equal(contestNameContestPool, contestNameBytes32);
+            assert.equal(startDateContestPool, startTime);
+            assert.equal(endDateContestPool, endTime);
+            assert.equal(maxBalanceContestPool, maxBalance);
+        };
+        await utils.assertEvent(instance, {
+            event: "CreateContestPool", args: {
+                contestName: contestNameBytes32
+            }
+        }, 1, callback);
+    });
+
+    it("Creating a contest pool instance using invalid contest name.", async function () {
+        const contestName = 'Rusia21';
+        try {
+            await instance.createContestPool(contestName, web3.toWei(2, 'ether'));
+            assert(false, 'It should fail because contest name is invalid.');
+        } catch (err) {
+            assert(err);
+        }
+    });
 });
