60cb7e18d760d867f86497af2246d88520781e38
dtutila <daniel.tutila@gmail.com>
2018-03-09 23:10:29 -0600

    updating format

--- test/DateUtil.js
+++ test/DateUtil.js
@@ -1,14 +1,14 @@
 const moment = require('moment');
 
 module.exports = {
-    toMoment: function(year, month, day) {
+    toMoment: function (year, month, day) {
         return moment(year + "-" + month + '-' + day);
     },
-    toDate: function(year, month, day) {
+    toDate: function (year, month, day) {
         return moment(year + "-" + month + '-' + day).toDate();
     },
-    toMillis: function(year, month, day) {
+    toMillis: function (year, month, day) {
         const date = year + "-" + month + '-' + day;
-        return moment(date, 'YYYY-MM-DD').toDate().getTime()/1000;
+        return moment(date, 'YYYY-MM-DD').toDate().getTime() / 1000;
     }
 };
--- test/StringUtil.js
+++ test/StringUtil.js
@@ -1,15 +1,15 @@
-const util = require('ethereumjs-util');    
+const util = require('ethereumjs-util');
 
 module.exports = {
-    cleanNulls: function(value) {
+    cleanNulls: function (value) {
         let newValue = value.replace('\0', '');
-        newValue = newValue.replace(/\0/g,'');
+        newValue = newValue.replace(/\0/g, '');
         return newValue;
-    },    
-    stringToBytes32: function(text) {
+    },
+    stringToBytes32: function (text) {
         return util.bufferToHex(util.setLengthRight(text, 32));
     },
-    stringToBytes8: function(text) {
+    stringToBytes8: function (text) {
         return util.bufferToHex(util.setLengthRight(text, 8));
     }
 };
