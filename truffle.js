c883152ec49ffa1686628709ed4e967de8c9bda8
Douglas Molina <doug.molina@coralbytes.com>
2018-03-16 10:14:24 -0600

    commenting network environment config to fix build

--- truffle.js
+++ truffle.js
@@ -1,18 +1,18 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  networks : {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*",
-      gas: 4500000
-    },
-    ganache: {
-      host: "localhost",
-      port: 7545,
-      network_id: "*",
-      gas: 4500000
-    }
-  }
+  // networks : {
+  //   development: {
+  //     host: "localhost",
+  //     port: 8545,
+  //     network_id: "*",
+  //     gas: 4500000
+  //   },
+  //   ganache: {
+  //     host: "localhost",
+  //     port: 7545,
+  //     network_id: "*",
+  //     gas: 4500000
+  //   }
+  // }
 };
