cfbb8c4e0d19b477cc251dcc72d3a2856852fa76
Douglas Molina <doug.molina@coralbytes.com>
2018-03-17 11:30:21 -0600

    refactoring ContestPoolTest to use factory needs fixing

--- test/ContestPoolTest.js
+++ test/ContestPoolTest.js
@@ -1,12 +1,17 @@
+const ContestPoolFactory = artifacts.require("./ContestPoolFactory.sol");
 const ContestPool = artifacts.require("./ContestPool.sol");
-const dateUtil = require('./utils/DateUtil');
-const t = require('./utils/TestUtil').title;
+
+const dateUtil = require('./DateUtil');
+const t = require('./TestUtil').title;
+const stringUtils = require('./StringUtil');
+
 
 // test suite
 contract('ContestPool', accounts => {
     let contestPoolInstance;
-    let owner = accounts[9];
-    let manager = accounts[0];
+    let contestPoolFactoryInstance;
+    let owner = accounts[0];
+    let manager = accounts[9];
     let player1 = accounts[1];
     let player2 = accounts[2];
     let player3 = accounts[3];
@@ -14,27 +19,73 @@ contract('ContestPool', accounts => {
     let startTime = dateUtil.toMillis(2018, 6, 14);
     let endTime = dateUtil.toMillis(2018, 7, 16);
     let graceTime = 1;
+    const fee = web3.toWei(0.01, 'ether');
     const maxBalance = web3.toWei(1, 'ether');
     const amountPerPlayer = web3.toWei(0.1, 'ether');
+
     const managerFee = 10;
     const ownerFee = 10;
 
-    beforeEach('setup contract for each test', async () => {
-        contestPoolInstance = await ContestPool.new(
-            owner,
-            manager,
-            "Rusia2018",
-            startTime,
-            endTime,
-            graceTime,
+    const contestName = stringUtils.uniqueText('Rusia2018');
+
+
+    before('setup suite', async () => {
+        contestPoolFactoryInstance = await ContestPoolFactory.deployed();
+
+        await contestPoolFactoryInstance.createContestPoolDefinition(
+            contestName, 
+            fee, 
+            startTime, 
+            endTime, 
+            graceTime, 
             maxBalance,
             amountPerPlayer,
             managerFee,
-            ownerFee
+            ownerFee,
+            { from : owner }
         );
+
+        let contestPoolAddress;
+        const tx = await contestPoolFactoryInstance.createContestPool(
+            contestName, 
+            amountPerPlayer,
+            {
+                from: manager,
+                value: fee
+            }
+        );
+        
+        contestPoolAddress = tx.logs[0].args.contestPoolAddress;
+        console.log('Contest Pool Address', contestPoolAddress);
+        constestPoolInstance = ContestPool(contestPoolAddress);
+
+        console.log("contest pool instance", contestPoolInstance);
+
+    });
+
+    beforeEach('setup contract for each test', async () => {
+
+        // contestPoolInstance = await ContestPool.new(
+        //     owner,
+        //     manager,
+        //     "Rusia2018",
+        //     startTime,
+        //     endTime,
+        //     graceTime,
+        //     maxBalance,
+        //     amountPerPlayer
+        // );
     })
 
-    it(t('AnyUser', 'new', 'Should be initialized with correct values'), async () => {
+    xit(t('AnyUser', 'setup', 'should have valid instance of ContestPoolFactory'), async () => {
+
+        assert(contestPoolFactoryInstance);
+        assert(contestPoolFactoryInstance.address);
+        assert(contestPoolInstance);
+        assert(contestPoolInstance.address);
+    });
+
+    xit(t('AnyUser', 'new', 'Should be initialized with correct values'), async () => {
         const startTimeContract = await contestPoolInstance.startTime();
         const endTimeContract = await contestPoolInstance.endTime();
         const graceTimeContract = await contestPoolInstance.graceTime();
